06f13ca1e9b206c934722191bc7d1dca48d05a0b
Nicaury Benitez <nbenitez@gmail.com>
2023-05-30 10:21:10 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from celery import Celery
 import os
 
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = os.getenv("CELERY_BROKER_URL")
-app.config['RESULT_BACKEND'] = os.getenv("CELERY_RESULT_BACKEND")
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+def create_app():
+    app = Flask(__name__)
+    app.config['CELERY_BROKER_URL'] = os.getenv("CELERY_BROKER_URL")
+    app.config['RESULT_BACKEND'] = os.getenv("CELERY_RESULT_BACKEND")
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 
-celery = Celery(app.import_name,
-                backend=app.config['RESULT_BACKEND'],
-                broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+    celery = Celery(app.import_name, backend=app.config['RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+
+    return app, celery
+
+app, celery = create_app()
