f55969bf517bce87df8ebea6c20c4035f8bafca3
Nicaury Benitez <nbenitez@gmail.com>
2023-05-30 10:22:44 +0200

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,10 +1,16 @@
 from app.app import celery
-import time
-import random
-
+from subprocess import Popen, PIPE
 
 @celery.task(name="report", acks_late=True)
 def report():
-    print("Generating report")
-    time.sleep(60)
-    return {"state":"completed"}
+    script_path = "<ruta_del_script>/nombre_del_script.py"  # Reemplaza "<ruta_del_script>" con la ruta real del script
+
+    process = Popen(["python", script_path], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        print("Error al ejecutar el script:")
+        print(stderr.decode())
+        return {"state": "error"}
+
+    return {"state": "completed"}
